a81da1057692a0e743a0034d7e8356c33530d4df
BigTex713 <ryancalp@gmail.com>
2025-09-20 11:01:40 -0400

    main.py word_count function

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ def get_book_text(book_file):
     with open(book_file) as f:
         book_contents = f.read()
         print(book_contents)
+
+def word_count(book_file):
+    with open(book_file) as f:
+        book_contents = f.read()
+        print(f"{len(book_contents.split())} words found in the document")
+
+
         
 def main():
     book = "books/frankenstein.txt"
-    return get_book_text(book)
+    return word_count(book)
     
 
 if __name__ == "__main__":

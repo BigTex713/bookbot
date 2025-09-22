839f10f348b813dcc25d641a8c49189559428b1c
BigTex713 <ryancalp@gmail.com>
2025-09-22 17:58:58 -0400

    added char_count function to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, char_count
 
 def get_book_text(book_file):
     with open(book_file) as f:
@@ -7,7 +7,8 @@ def get_book_text(book_file):
         
 def main():
     book = "books/frankenstein.txt"
-    return word_count(book)
+    print(word_count(book) )
+    print(char_count(book))
     
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def word_count(book_file):
     with open(book_file) as f:
         book_contents = f.read()
         print(f"{len(book_contents.split())} words found in the document")
+        
+def char_count(book_file):
+    ltr_cnt_dict = {}
+    
+    with open(book_file) as f:
+        book_contents = f.read()
+        
+        for ltr in book_contents.lower():
+            if ltr in ltr_cnt_dict:
+                ltr_cnt_dict[ltr] += 1
+            else:
+                ltr_cnt_dict[ltr] = 1
+    return ltr_cnt_dict

597e400130bc87e37bb711200e16ca4102087486
BigTex713 <ryancalp@gmail.com>
2025-09-30 20:20:40 -0400

    working on sys.arg addition to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count, char_count, dict_list, sort_on
+import sys
 
 def get_book_text(book_file):
     with open(book_file) as f:
@@ -6,22 +7,32 @@ def get_book_text(book_file):
         print(book_contents)
         
 def main():
-    book = "books/frankenstein.txt"
+    args = sys.argv
+    print(sys.argv)
     
-    book_dict = char_count(book)
-    book_dict_list = dict_list(book_dict)
-    book_dict_list.sort(reverse=True, key=sort_on)
-    dict_access_key = 0 #will use this to set the position in the dictionay list
-  
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")  
-    print(word_count(book))
-    print("--------- Character Count -------")
-    for ltr_dict in book_dict_list:
-        print(f"{book_dict_list[dict_access_key]['char']}: {book_dict_list[dict_access_key]['num']}")
-        dict_access_key +=1
-    print("============= END ===============")
+    book = args
+    
+    if len(book) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    else:       
+        book_dict = char_count(book[1])
+        book_dict_list = dict_list(book_dict)
+        book_dict_list.sort(reverse=True, key=sort_on)
+        dict_access_key = 0 #will use this to set the position in the dictionay list
+    
+
+
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")  
+        print(word_count(book))
+        print("--------- Character Count -------")
+        for ltr_dict in book_dict_list:
+            print(f"{book_dict_list[dict_access_key]['char']}: {book_dict_list[dict_access_key]['num']}")
+            dict_access_key +=1
+        print("============= END ===============")
 
 if __name__ == "__main__":
     main()
